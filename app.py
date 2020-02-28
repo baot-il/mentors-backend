8be87178217546ac716bceb8ad8221abb4700058
ophirharpaz <ophirharpaz@gmail.com>
2020-02-28 20:16:22 +0200

    Add /technologies API

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def hello():
     return "Hello World!"
 
 
+@app.route('/technologies')
+def get_technologies():
+    return {'technologies': ['Python', 'NodeJS', 'JavaScript', 'Machine Learning', 'TensorFlow']}
+
+
 if __name__ == '__main__':
     app.run()
