88c8d10cfb5ce968acbac234bba86e88e639bb80
ophirharpaz <ophirharpaz@gmail.com>
2020-02-28 22:37:56 +0200

    Remove dead code and fix an import

--- extensions.py
+++ extensions.py
@@ -4,7 +4,3 @@ from flask_sqlalchemy import SQLAlchemy
 
 migrate = Migrate()
 db = SQLAlchemy()
-
-
-def get_all_technologies():
-    return
--- models/models.py
+++ models/models.py
@@ -1,4 +1,4 @@
-from app import db
+from extensions import db
 import datetime as dt
 
 
