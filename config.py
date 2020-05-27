570873494cbb11e3e49e61354fb3398430ca3950
Ilana Kantorov <ilanak@microsoft.com>
2020-05-27 11:15:56 +0300

    Skip authenticate validation on development

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
+    DEVELOPMENT = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -19,7 +20,6 @@ class ProductionConfig(Config):
 
 
 class StagingConfig(Config):
-    DEVELOPMENT = True
     DEBUG = True
 
 
