2137ec73e203304bb91a1c7a7a453b0787de5f3c
Elvira Gandelman <elvirag@gmail.com>
2020-06-06 23:27:15 +0300

    Added migrations to app + reference on how2do (a link)

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
+import os
+from urllib.parse import uses_netloc, urlparse
+
 from flask import Flask
 from flask_cors import CORS
-from peewee import PostgresqlDatabase
-from urllib.parse import uses_netloc, urlparse
-import os
+from peewee import PostgresqlDatabase, BigIntegerField
+from playhouse.migrate import PostgresqlMigrator, migrate
 
 uses_netloc.append('postgres')
 url = urlparse(os.environ['DATABASE_URL'])
 
 DATABASE = {
-    'engine': 'peewee.PostgresqlDatabase',
-    'name': url.path[1:],
-    'password': url.password,
-    'host': url.hostname,
-    'port': url.port,
+	'engine': 'peewee.PostgresqlDatabase',
+	'name': url.path[1:],
+	'password': url.password,
+	'host': url.hostname,
+	'port': url.port,
 }
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-db = PostgresqlDatabase(database=url.path[1:],
-                        user=url.username,
-                        password=url.password,
-                        host=url.hostname,
-                        port=url.port)
+db = PostgresqlDatabase(
+	database=url.path[1:],
+	user=url.username,
+	password=url.password,
+	host=url.hostname,
+	port=url.port
+)
+
+# reference: https://peewee.readthedocs.io/en/latest/peewee/playhouse.html#schema-migrations
+migrator = PostgresqlMigrator(db)
+migrate(
+	migrator.alter_column_type('mentee', 'phone', BigIntegerField()),
+	migrator.alter_column_type('mentor', 'phone', BigIntegerField())
+)
 cors = CORS(app)
 
-
 if __name__ == '__main__':
-    app.run()
+	app.run()
